d57fe6ecae598d7fd4f0186739d9d9b3222e8591
Ioan Bizău <i@ibz.me>
2021-12-09 08:47:07 +0200

    Use forced mode and ts.

--- read_bme280.py
+++ read_bme280.py
@@ -9,6 +9,8 @@ from smbus import SMBus
 bus = SMBus(1)
 bme280 = BME280(i2c_dev=bus)
 
+bme280.setup(mode="forced")
+
 now = datetime.now()
 temperature = bme280.get_temperature()
 pressure = bme280.get_pressure()
@@ -18,8 +20,8 @@ filename = now.strftime("%Y-%m-%d.csv")
 
 if not os.path.exists(filename):
     with open(filename, 'w') as f:
-        f.write("h,m,temperature,pressure,humidity\n")
+        f.write("ts,temperature,pressure,humidity\n")
 
 with open(filename, 'a') as f:
-    f.write("%d,%d,%.2f,%.2f,%.2f\n" % (now.hour, now.minute, temperature, pressure, humidity))
+    f.write("%d,%.2f,%.2f,%.2f\n" % (now.timestamp(), temperature, pressure, humidity))
 
